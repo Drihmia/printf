dfe369f2df2109a86cad4f2ae755305a65522d07
Drihmia <drihmia.redouane@gmail.com>
2023-09-16 21:01:36 +0100

    header file

--- main.h
+++ main.h
@@ -7,4 +7,10 @@
 
 int _printf(const char *format, ...);
 int _putchar(char c);
+void print_number(int n);
+int _len(int n);
+int _pow(int m, int n);
+void print_unumber(unsigned int n);
+int _ulen(unsigned int n);
+unsigned int _upow(int m, int n);
 #endif
