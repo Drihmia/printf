a42e48862b57bc75d39d75937bb7ac074a8dc8f2
Drihmia <drihmia.redouane@gmail.com>
2023-09-16 23:55:39 +0100

    added a case where i check for format is not null and the formt is not empty

--- _printf.c
+++ _printf.c
@@ -11,28 +11,30 @@ int _printf(const char *format, ...)
 	va_list list;
 	const char *i = format;
 	char *arg;
-
-	va_start(list, format);
-	for (; *i != '\0'; i++)
+	if (format != NULL && format[0] !='\’n')
 	{
-		if (*i == '%')
+		va_start(list, format);
+		for (; *i != '\0'; i++)
 		{
-			if (*(i + 1) == 'c')
-				putchar(va_arg(list, int)), i++;
-			else if (*(i + 1) == 's')
-				arg = va_arg(list, char *), write(1, arg, strlen(arg)), i++;
-			else if (*(i + 1) == '%')
-				putchar('%'), i++;
-			else if (*(i + 1) == 'd' || *(i + 1) == 'i')
-				print_number(va_arg(list, int)), i++;
-			else if (*(i + 1) == 'u')
-				print_unumber(va_arg(list, int)), i++;
+			if (*i == '%')
+			{
+				if (*(i + 1) == 'c')
+					putchar(va_arg(list, int)), i++;
+				else if (*(i + 1) == 's')
+					arg = va_arg(list, char *), write(1, arg, strlen(arg)), i++;
+				else if (*(i + 1) == '%')
+					putchar('%'), i++;
+				else if (*(i + 1) == 'd' || *(i + 1) == 'i')
+					print_number(va_arg(list, int)), i++;
+				else if (*(i + 1) == 'u')
+					print_unumber(va_arg(list, int)), i++;
+				else
+					exit(EXIT_FAILURE);
+			}
 			else
-				exit(EXIT_FAILURE);
+				putchar(*i);
 		}
-		else
-			putchar(*i);
+		va_end(list);
 	}
-	va_end(list);
 	return (strlen(format));
 }
