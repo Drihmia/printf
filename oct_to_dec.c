4e6c0b863900c3b448c3897918caf207b7ed2eae
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-17 06:02:25 -0700

    This is the function that convert the octal to the decimal in separated file

---
+++ oct_to_dec.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * oct_to_dec - this function convert octal to decimal number
+ * @n: the number that we want to convert
+ * Return: it return decimal number
+ */
+
+int oct_to_dec(int n)
+{
+int s, i = 0;
+int sum = 0;
+while (n > 0)
+	{
+	s = n % 10;
+	n = n / 10;
+	sum += pow(8, i) * s;
+	i++;
+	}
+return (sum);
+}
