0fb1845a564670a792690b011736e03d9aa98dd0
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-18 05:53:49 -0700

    I create another function that convert from the decimal to the hexadecimal but this time in lowercase that named dec_to_hexa

--- dec_to_bina.c
+++ dec_to_bina.c
@@ -8,7 +8,7 @@
 
 int dec_to_bina(unsigned int n)
 {
-	unsigned s, i = 0, j = 1;
+unsigned int s, i = 0, j = 1;
 	while (n > 0)
 	{
 		s = n % 2;
@@ -20,24 +20,48 @@ int dec_to_bina(unsigned int n)
 }
 
 /**
- * DEC_TO_HEXA - this function convert a decimal number to an hexa with uppercase
+ * DEC_TO_HEXA - this function convert a decimal num to an hexa with uppercase
  * @decimal_number: the decimal number
  */
 
-void DEC_TO_HEXA(int decimal_Number)
+void DEC_TO_HEXA(int decimal_number)
 {
-	int i = 1, j, temp;
-	char hexa_Number[100];
-
-	while (decimal_Number != 0) {
-		temp = decimal_Number % 16;
+int i = 0, j, temp;
+char hexa_number[100];
+	while (decimal_number != 0)
+	{
+		temp = decimal_number % 16;
 		if (temp < 10)
 			temp = temp + 48;
 		else
 			temp = temp + 55;
-		hexa_Number[i++] = temp;
-		decimal_Number = decimal_Number / 16;
+		hexa_number[i] = temp, i++;
+		decimal_number = decimal_number / 16;
 	}
-	for (j = i - 1; j > 0; j--)
-		putchar(hexa_Number[j]);
+	for (j = i - 1; j >= 0; j--)
+		putchar(hexa_number[j]);
+}
+
+/**
+ * dec_to_hexa - this function convert a decimal num to an hexa with lowercase
+ * @decimal_number: the decimal number
+ */
+
+void dec_to_hexa(int decimal_number)
+{
+	int i = 0, j, temp;
+	char hexa_number[100];
+
+	while (decimal_number != 0)
+	{
+		temp = decimal_number % 16;
+		if (temp < 10)
+			temp = temp + 48;
+		else
+			temp = temp + 87;
+		hexa_number[i] = temp, i++;
+		decimal_number = decimal_number / 16;
+	}
+	for (j = i - 1; j >= 0; j--)
+		putchar(hexa_number[j]);
 }
