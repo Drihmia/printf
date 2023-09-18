900c9f055de03fc1d7882c239bf848eead326bed
Hicham Bourezi <Hicham.bourezi@gmail.com>
2023-09-18 14:36:50 -0700

    I just fixed the first function that convert to hexa in uppercase

--- dec_to_bina.c
+++ dec_to_bina.c
@@ -5,44 +5,47 @@
  * @decimal_number: the decimal number
  */
 
-void DEC_TO_HEXA(int decimal_number)
+char * DEC_TO_HEXA(int decimal_number)
 {
-int i = 0, j, temp;
-char hexa_number[100];
-	while (decimal_number != 0)
+	int i = 0, j, tmp = decimal_number, start, end;
+	char *hexa_number, temp;
+
+	if (decimal_number == 0)
 	{
-		temp = decimal_number % 16;
-		if (temp < 10)
-			temp = temp + 48;
-		else
-			temp = temp + 55;
-		hexa_number[i] = temp, i++;
+		hexa_number = malloc(sizeof(char) * 2);
+		if (hexa_number == NULL)
+			return (NULL);
+		hexa_number[0] = '0';
+		hexa_number[1] = '\0';
+		return (hexa_number);
+	}
+
+	while (tmp != 0)
+	{
+		tmp = tmp / 16;
+		i++;
+	}
+
+	hexa_number = malloc(sizeof(char)*(i + 1));
+	if (hexa_number == NULL)
+		return (NULL);
+	for (j = 0; j < i; j++)
+	{
+		tmp = decimal_number % 16;
+		hexa_number[j] = (tmp < 10) ? tmp + '0' : tmp + 'A' - 10;
 		decimal_number = decimal_number / 16;
 	}
-	for (j = i - 1; j >= 0; j--)
-		putchar(hexa_number[j]);
+	hexa_number[j] = '\0';
+	for (start = 0, end = j - 1; start < end; start++, end--)
+	{
+		temp = hexa_number[start];
+		hexa_number[start] = hexa_number[end];
+		hexa_number[end] = temp;
+	}
+	return (hexa_number);
 }
 
 /**
  * dec_to_hexa - this function convert a decimal num to an hexa with lowercase
  * @decimal_number: the decimal number
  */
-
-void dec_to_hexa(int decimal_number)
-{
-	int i = 0, j, temp;
-	char hexa_number[100];
-
-	while (decimal_number != 0)
-	{
-		temp = decimal_number % 16;
-		if (temp < 10)
-			temp = temp + 48;
-		else
-			temp = temp + 87;
-		hexa_number[i] = temp, i++;
-		decimal_number = decimal_number / 16;
-	}
-	for (j = i - 1; j >= 0; j--)
-		putchar(hexa_number[j]);
-}
