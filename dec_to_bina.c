f6320dadaaf779b59cc4d678641dd5b7bb23f841
Drihmia <drihmia.redouane@gmail.com>
2023-09-19 12:48:30 +0100

    return 1 instead zero

--- dec_to_bina.c
+++ dec_to_bina.c
@@ -15,7 +15,7 @@ int DEC_TO_HEXA(unsigned int decimal_number)
 	if (decimal_number == 0)
 	{
 		putchar('0');
-		return (0);
+		return (1);
 	}
 
 	while (tmp != 0)
@@ -61,7 +61,7 @@ int dec_to_hexa(unsigned int decimal_number)
 	if (decimal_number == 0)
 	{
 		putchar('0');
-		return (0);
+		return (1);
 	}
 
 	while (tmp != 0)
