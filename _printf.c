7d9cd5a09451f4aa1829458aa437b89b4823c1fc
Drihmia <drihmia.redouane@gmail.com>
2023-09-19 08:44:52 +0100

    remived a unecessery if

--- _printf.c
+++ _printf.c
@@ -14,38 +14,35 @@ int _printf(const char *format, ...)
 
 	if (format == NULL)
 		return (-1);
-	if (format[0] != '\0')
+	va_start(list, format);
+	for (; *i != '\0'; i++)
 	{
-		va_start(list, format);
-		for (; *i != '\0'; i++)
+		if (*i == '%')
 		{
-			if (*i == '%')
-			{
-				if (*(i + 1) == 'c')
-					len_pr += print_char(va_arg(list, int)), i++;
-				else if (*(i + 1) == 's')
-					len_pr += print_str(va_arg(list, char *)), i++;
-				else if (*(i + 1) == '%')
-					putchar('%'), i++;
-				else if (*(i + 1) == 'd' || *(i + 1) == 'i' || *(i + 1) == 'u')
-					len_pr += print_numbers(list, *(i + 1)), i++;
-				else if (*(i + 1) == 'O')
-					len_pr += print_number(oct(va_arg(list, int))), i++;
-				else if (*(i + 1) == 'b')
-					len_pr += dec_to_bin(va_arg(list, int)), i++;
-				else if (*(i + 1) == 'X')
-					len_pr += DEC_TO_HEXA(va_arg(list, int)), i++;
-				else if (*(i + 1) == 'x')
-					len_pr += dec_to_hexa(va_arg(list, int)), i++;
-				else if (*(i + 1) == 'p')
-					i++;
-				else
-					return (-1);
-			}
+			if (*(i + 1) == 'c')
+				len_pr += print_char(va_arg(list, int)), i++;
+			else if (*(i + 1) == 's')
+				len_pr += print_str(va_arg(list, char *)), i++;
+			else if (*(i + 1) == '%')
+				putchar('%'), i++;
+			else if (*(i + 1) == 'd' || *(i + 1) == 'i' || *(i + 1) == 'u')
+				len_pr += print_numbers(list, *(i + 1)), i++;
+			else if (*(i + 1) == 'O')
+				len_pr += print_number(oct(va_arg(list, int))), i++;
+			else if (*(i + 1) == 'b')
+				len_pr += dec_to_bin(va_arg(list, int)), i++;
+			else if (*(i + 1) == 'X')
+				len_pr += DEC_TO_HEXA(va_arg(list, int)), i++;
+			else if (*(i + 1) == 'x')
+				len_pr += dec_to_hexa(va_arg(list, int)), i++;
+			else if (*(i + 1) == 'p')
+				i++;
 			else
-				putchar(*i);
+				return (-1);
 		}
-		va_end(list);
+		else
+			putchar(*i);
 	}
+	va_end(list);
 	return (f_l + len_pr);
 }
