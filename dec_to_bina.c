70c4a67db8f948ee5b97591d89147329a96e44cb
Drihmia <drihmia.redouane@gmail.com>
2023-09-19 21:28:53 +0100

    return zero if str in NULL

--- dec_to_bina.c
+++ dec_to_bina.c
@@ -103,7 +103,7 @@ int convertS(char *string)
 	int i, j = 0;
 
 	if (string == NULL)
-		return (1);
+		return (0);
 	for (i = 0; string[i] != '\0'; i++)
 	{
 		if ((*(string + i) < 32) || (*(string + i) >= 127))
